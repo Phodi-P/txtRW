945dc2fe49df86ed9cd25d5b29f820e7363db623
Phodi-P <5670253+Phodi-P@users.noreply.github.com>
2019-05-05 20:33:52 +0700

    append line example

--- txtRW.cpp
+++ txtRW.cpp
@@ -8,6 +8,12 @@ int main()
 {
     txtRW txtRW;
     txtRW.writeline("input.txt",2,txtRW.readline("input.txt",2)+" Editted");
-    cout << txtRW.readline("input.txt",2);
+    txtRW.appendline("input.txt",0," editted");
+    txtRW.appendline("input.txt",99," editted");
+    txtRW.appendline("input.txt",99," editted2");
+    txtRW.writeline("input.txt",0,"Something");
+    txtRW.writeline("input.txt",3,"Something1");
+    txtRW.writeline("input.txt",100,"Something1");
+    cout << txtRW.readline("input.txt",0);
     return 0;
 }
